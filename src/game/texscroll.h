64a0caf1ec543f7fd99b92e036219052477df427
Luigiman04 <root@LogansPC>
2024-04-06 18:31:05 -0400

    Title Screen

--- src/game/texscroll.h
+++ src/game/texscroll.h
@@ -12,6 +12,7 @@
 #include "src/game/texscroll/ssl_texscroll.inc.h"
 #include "src/game/texscroll/castle_grounds_texscroll.inc.h"
 #include "src/game/texscroll/castle_inside_texscroll.inc.h"
+#include "src/game/texscroll/intro_texscroll.inc.h"
 extern void scroll_textures();
 
 #endif
